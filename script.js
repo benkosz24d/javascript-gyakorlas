b53ba791db0b876918e376936801003aab71f6a4
benkosz24d <benkosz.24d@acsjszki.hu>
2025-03-18 08:40:53 +0100

    633

---
+++ script.js
@@ -0,0 +1,20 @@
+// arra of <number>
+var szamok = [2, 4, 13, 4, 6, 42];
+
+var paroskaSzama = 0;
+
+paroskaSzama += szamok[0] % 2 === 0? 1 : 0;
+paroskaSzama += szamok[1] % 2 === 0? 1 : 0; 
+paroskaSzama += szamok[2] % 2 === 0? 1 : 0;
+paroskaSzama += szamok[3] % 2 === 0? 1 : 0;
+paroskaSzama += szamok[4] % 2 === 0? 1 : 0;
+paroskaSzama += szamok[5] % 2 === 0? 1 : 0;
+
+
+console.log(paroskaSzama);
+
+
+while(true) {
+    // console.log("vegtelen ciklus...");
+
+}
