c2461b83177e76bcd7c4d522aff77b3d13c653b7
benkosz24d <benkosz.24d@acsjszki.hu>
2025-03-19 14:19:27 +0100

    18:00

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // arra of <number>
-var szamok = [2, 4, 13, 4, 6, 42];
+var szamok = [2, 4, 13, 4, 6, 42, 3];
 
 var paroskaSzama = 0;
 
@@ -14,7 +14,32 @@ paroskaSzama += szamok[5] % 2 === 0? 1 : 0;
 console.log(paroskaSzama);
 
 var index = 0;
-while(index < 6) {
-     console.log(index);
+var paratlanokSzama = 0;
+while(index < szamok.length) {
+    
     index++;
 }
+//console.log(paratlanokSzama);
+
+
+// for loop
+var paratlanokSzama2 = 0;
+for(var i=0; i< szamok.length; i++) {
+    //console.log(i);
+    //console.log(szamok[i]);
+    //console.log('------- ');
+    paratlanokSzama += szamok[i] % 2 === 0 ? 1 :  0;
+}
+
+console.log(paratlanokSzama2);
+
+//for - of
+
+for(var szam of szamok)
+
+
+
+
+ 
+
+
